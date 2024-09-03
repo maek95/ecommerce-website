c560ba7691dc6dc951bc1d850f3e87870fda51bd
Mikael-Chasacademy <mikael.linderoth@chasacademy.se>
2024-09-03 14:28:21 +0200

    Product page

--- src/app/product-page/page.jsx
+++ src/app/product-page/page.jsx
@@ -5,17 +5,20 @@ export default function productpage() {
   //const productpage = () => {};
 
   return (
-    <main className="flex min-h-screen flex-col pl-4 pr-4">
-      <nav className="bg-blue-500">
-        <h1>logo </h1>
+    <main className="bg-green-400 flex min-h-screen flex-col pl-4 pr-4">
+      <nav className="bg-orange-500 ">
+        <h1 className="text-blue-800 text-center">Placeholder Navbar </h1>
       </nav>
       <div>
         <h1>product page</h1>
-        <header>
-          <h1>Product Bild</h1>
+        <header className="bg-white border-solid border-2 border-black">
+          <img
+            className="h-full max-w-full object-cover"
+            alt="Jack &amp; Jones Plus Size Teddy jacket -Black - 12263083"
+            src="https://images.jackjones.com/12263083/4573159/001/jackjones-plussizeteddyjacket-black.png?v=681cb536087b86d48a22e75070fb4bf3&amp;format=webp&amp;width=1280&amp;quality=90&amp;key=25-0-3"
+          ></img>
         </header>
         <div>
-          <h1>product info</h1>
           <div>
             <p>product name</p>
           </div>
@@ -23,7 +26,7 @@ export default function productpage() {
             <p>product price</p>
           </div>
           <div>
-            <p>add to basket</p>
+            <button>add to basket</button>
           </div>
           <div>
             <p>product usp</p>
@@ -33,7 +36,7 @@ export default function productpage() {
           </div>
         </div>
       </div>
-      <footer>
+      <footer className="bg-orange-500 ">
         <p>Footer</p>
       </footer>
     </main>
