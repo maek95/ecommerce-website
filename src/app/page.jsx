84ba0c776cad820d3abf2f0a3f0f81ce952f9653
maek95 <mekstrom01@gmail.com>
2024-09-03 13:17:04 +0200

    remove styling and stuff

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <h1 className="text-blue-800">JACK&JONES</h1>
+    </main>
+  );
+}
