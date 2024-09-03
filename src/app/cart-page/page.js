b28818cabfb1439f04e24ba62b75ebaf9620d8cf
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-09-03 13:27:49 +0200

    mockdata and changes in cart-page

---
+++ src/app/cart-page/page.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function CartPage() {
+  const [cart, setCart] = useState("not empty");
+  const [mockCart, setMockCart] = useState([]);
+
+  if (cart === "empty") {
+    return <div>Your cart is empty</div>;
+  }
+
+  useEffect(() => {
+    async function getMockData() {
+      try {
+        const response = await fetch("/mockdata.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const mockData = await response.json();
+        setMockCart(mockData);
+      } catch (error) {
+        console.error("Failed to fetch mockdata:", error);
+      }
+    }
+    getMockData();
+  }, []);
+
+  return (
+    <main>
+      <h1>Cart</h1>
+      <ul>
+        {mockCart.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            {item.color}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
