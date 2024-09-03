49320383c98dbbc08f650c479cc3594ace0f3fd7
maek95 <mekstrom01@gmail.com>
2024-09-03 13:59:23 +0200

    font-bebas tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        //sourceSans: ['"Bebas Neue"', "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
