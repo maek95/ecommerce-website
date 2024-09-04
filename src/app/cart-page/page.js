ced36a8fceafc1eb9727f7d46d75951ff57d75d4
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-09-04 11:19:18 +0200

    tested fetch in cart-page

--- src/app/cart-page/page.js
+++ src/app/cart-page/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function CartPage() {
   const [cart, setCart] = useState("not empty");
@@ -13,12 +14,15 @@ export default function CartPage() {
   useEffect(() => {
     async function getMockData() {
       try {
-        const response = await fetch("/mockdata.json");
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const mockData = await response.json();
         setMockCart(mockData);
+        console.log(mockData);
       } catch (error) {
         console.error("Failed to fetch mockdata:", error);
       }
@@ -27,15 +31,20 @@ export default function CartPage() {
   }, []);
 
   return (
-    <main>
-      <h1>Cart</h1>
-      <ul>
+    <main className="min-h-screen w-full px-8 box-border">
+      <Navbar />
+      <h1 className="text-[#0468db] text-3xl">Cart</h1>
+      <ul className="p-0">
         {mockCart.map((item) => (
-          <li key={item.id}>
-            {item.name}
-            {item.color}
-            <img src="mock-img.webp" className="w-24"></img>
-          </li>
+          <div key={item.id} className="list-none flex">
+            {item.images && item.images.length > 0 && (
+              <img src={item.images[0]} alt={item.title} className="w-24" />
+            )}
+            <li className="flex flex-col ml-4">
+              <span>{item.title}</span>
+              <span>{item.price} USD</span>
+            </li>
+          </div>
         ))}
       </ul>
     </main>
